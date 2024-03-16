fa3d03a669c5a70b7e5cbd2018db43d918d82037
roblesdotdev <httpsnip@gmail.com>
2024-03-16 16:51:28 -0300

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import "~/styles/tailwind.css";
 import {
   Links,
   Meta,
@@ -15,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="antialiased">
         {children}
         <ScrollRestoration />
         <Scripts />
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+export default {
+  content: ["./app/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
