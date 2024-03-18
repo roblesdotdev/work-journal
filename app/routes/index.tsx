b216d6714457aba53927bdbb0f632b192f078528
roblesdotdev <httpsnip@gmail.com>
2024-03-18 18:36:49 -0300

    displaying entries by week

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import type { Record } from '@prisma/client/runtime/library'
 import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node'
 import {
   Form,
@@ -7,7 +8,7 @@ import {
   useLoaderData,
   useNavigation,
 } from '@remix-run/react'
-import { format } from 'date-fns'
+import { format, parseISO, startOfWeek } from 'date-fns'
 import { useEffect, useRef, useState } from 'react'
 import { z } from 'zod'
 import { db } from '~/utils/db.server'
@@ -23,7 +24,10 @@ export async function loader() {
   const entries = await db.entry.findMany()
 
   return json({
-    entries,
+    entries: entries.map(entry => ({
+      ...entry,
+      date: entry.date.toISOString().substring(0, 10), // remove tz
+    })),
   })
 }
 
@@ -81,6 +85,31 @@ export default function Index() {
 
   const isHydrated = useHydrated()
   const isLoading = navigation.state !== 'idle'
+
+  const entriesByWeek = data.entries.reduce<
+    Record<string, typeof data.entries>
+  >((memo, entry) => {
+    const sunday = startOfWeek(parseISO(entry.date))
+    const sundayStr = format(sunday, 'yyyy-MM-dd')
+
+    memo[sundayStr] ||= []
+    memo[sundayStr].push(entry)
+
+    return memo
+  }, {})
+
+  const weeks = Object.keys(entriesByWeek)
+    .sort((a, b) => a.localeCompare(b))
+    .map(dateString => ({
+      dateString,
+      work: entriesByWeek[dateString].filter(entry => entry.type === 'work'),
+      learnings: entriesByWeek[dateString].filter(
+        entry => entry.type === 'learning',
+      ),
+      interestingThings: entriesByWeek[dateString].filter(
+        entry => entry.type === 'interest-things',
+      ),
+    }))
 
   useEffect(() => {
     if (!isLoading && contentRef.current) {
@@ -191,7 +220,53 @@ export default function Index() {
           </fieldset>
         </Form>
       </div>
-      <pre>{JSON.stringify(data.entries, null, 2)}</pre>
+      <div className="mt-12 space-y-8">
+        {weeks.length ? (
+          weeks.map(week => (
+            <div key={week.dateString}>
+              <p className="font-bold">
+                Week of {format(parseISO(week.dateString), 'MMMM do')}
+              </p>
+              {week.work.length ? (
+                <div>
+                  <p className="mt-2 font-medium">Work</p>
+                  <ul className="ml-8 list-disc">
+                    {week.work.map(entry => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+              {week.learnings.length ? (
+                <div>
+                  <p className="mt-2 font-medium">Learning</p>
+                  <ul className="ml-8 list-disc">
+                    {week.learnings.map(entry => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+              {week.interestingThings.length ? (
+                <div>
+                  <p className="mt-2 font-medium">Interesting things</p>
+                  <ul className="ml-8 list-disc">
+                    {week.interestingThings.map(entry => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>
+            <p className="text-lg text-gray-400">
+              Not entries yet. Try creating one.
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
